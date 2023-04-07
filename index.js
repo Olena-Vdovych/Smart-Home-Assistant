5313840df37a1c33b2ad837cadf716d51817da90
Olena Vdovych <olena.vdovych2@gmail.com>
2023-04-07 14:06:20 +0300

    Added functionality for sound system input

--- index.js
+++ index.js
@@ -54,9 +54,24 @@ appliancesToogle.addEventListener('click', function () {
 // Air conditioner input
 const acTemperatureInput = document.querySelector('.ac-temperature');
 const conditionerToggle = document.querySelector('.conditioner-toggle');
-const text = document.querySelector('.text-conditioner');
+const textConditioner = document.querySelector('.text-conditioner');
 
 acTemperatureInput.addEventListener('input', () => {
     const percent = ((acTemperatureInput.value - acTemperatureInput.min) / (acTemperatureInput.max - acTemperatureInput.min) * 100).toFixed(0);
-    text.textContent = `${percent}%`;
+    textConditioner.textContent = `${percent}%`;
+});
+
+// Sound system input
+const soundSystemInput = document.querySelector('.sound-system-volume');
+const soundSystemToggle = document.querySelector('.sound-toggle');
+const textSoundSystem = document.querySelector('.text-sound');
+
+let previousVolume = 50;
+soundSystemInput.addEventListener('input', () => {
+    const newVolume = parseInt(soundSystemInput.value, 10);
+    const volumeDifference = newVolume - previousVolume;
+    const direction = volumeDifference > 0 ? 'is increasing' : 'is decreasing';
+
+    textSoundSystem.textContent = `Sound ${direction} on ${Math.abs(volumeDifference)}% (volume level: ${newVolume})`;
+    previousVolume = newVolume;
 });
