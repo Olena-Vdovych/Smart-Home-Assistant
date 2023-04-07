816197df8579e0401ec943182566125047633d4d
Olena Vdovych <olena.vdovych2@gmail.com>
2023-04-07 12:53:58 +0300

    Changed buttons on/off, added styles

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-const buttons = document.querySelectorAll('.device-switch');
+const lightToggle = document.querySelector('.light-toggle');
+const conditionerToogle = document.querySelector('.conditioner-toggle');
+const tvToggle = document.querySelector('.tv-toggle');
+const soundToggle = document.querySelector('.sound-toggle');
+const cookerToogle = document.querySelector('.cooker-toogle');
+const jalousieToogle = document.querySelector('.jalousie-toggle');
+const securityToggle = document.querySelector('.security-toggle');
+const lightsToggle = document.querySelector('.lights-toggle');
+const appliancesToogle = document.querySelector('.appliances-toogle');
+let active = false;
 
-buttons.forEach(button => {
-    // Встановлюємо початковий стан кнопки
-    const deviceStatus = button.parentElement.classList.contains('on') ? 'on' : 'off';
-    button.classList.add(deviceStatus);
+function toggle(element) {
+    let toggle = element.querySelector('.toggle');
+    let text = element.querySelector('.text');
+    let active = toggle.classList.contains('active');
+    if (!active) {
+        toggle.classList.add('active');
+        text.innerHTML = 'ON';
+    } else {
+        toggle.classList.remove('active');
+        text.innerHTML = 'OFF';
+    }
+}
 
-    // Додаємо обробник події click на кожну кнопку
-    button.addEventListener('click', () => {
-        button.classList.toggle('on');
-        button.classList.toggle('off');
-    });
+lightToggle.addEventListener('click', function () {
+    toggle(document.querySelector('#light'));
+});
+conditionerToogle.addEventListener('click', function () {
+    toggle(document.querySelector('#ac'));
+});
+tvToggle.addEventListener('click', function () {
+    toggle(document.querySelector('#tv'));
+});
+soundToggle.addEventListener('click', function () {
+    toggle(document.querySelector('#sound-system'));
+});
+cookerToogle.addEventListener('click', function () {
+    toggle(document.querySelector('#cooker'));
+});
+jalousieToogle.addEventListener('click', function () {
+    toggle(document.querySelector('#jalousie'));
+});
+securityToggle.addEventListener('click', function () {
+    toggle(document.querySelector('#security'));
+});
+lightsToggle.addEventListener('click', function () {
+    toggle(document.querySelector('#lights'));
+});
+appliancesToogle.addEventListener('click', function () {
+    toggle(document.querySelector('#appliances'));
 });
