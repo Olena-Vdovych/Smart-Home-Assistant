fec5875e280170515e2fdf3d6e207174908899d8
Olena Vdovych <olena.vdovych2@gmail.com>
2023-04-14 17:01:36 +0300

    Changed alarm clock

--- js/alarm-clock.js
+++ js/alarm-clock.js
@@ -1,5 +1,8 @@
-document.getElementById("clock-submit").addEventListener("click", function () {
+document.getElementById("clock-submit").addEventListener("click", function (event) {
+    event.preventDefault();
     let time = document.getElementById('time-input').value;
     let melody = document.getElementById('melody-select').value;
+
     document.getElementById("bedroom-container").innerHTML = `Alarm clock at ${time}, alarm ${melody}`;
 });
+
