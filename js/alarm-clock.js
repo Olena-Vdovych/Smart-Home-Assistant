1d880dc8462f62092ba9f4e7b11556abc2e3efc1
Olena Vdovych <olena.vdovych2@gmail.com>
2023-04-14 17:55:54 +0300

    Added rooms for alarm clock

--- js/alarm-clock.js
+++ js/alarm-clock.js
@@ -1,8 +1,19 @@
-document.getElementById("clock-submit").addEventListener("click", function (event) {
-    event.preventDefault();
+const bedroomContainer = document.getElementById('bedroom-container');
+const livingRoomContainer = document.getElementById('living-room-container');
+const kitchenContainer = document.getElementById('kitchen-container');
+
+document.getElementById('clock-submit').addEventListener('click', function (e) {
+    e.preventDefault();
+    let room = document.getElementById('room-select').value;
     let time = document.getElementById('time-input').value;
     let melody = document.getElementById('melody-select').value;
 
-    document.getElementById("bedroom-container").innerHTML = `Alarm clock at ${time}, alarm ${melody}`;
+    if (room === 'bedroom') {
+        bedroomContainer.innerHTML = `Alarm clock at ${time}, alarm ${melody}`;
+    } else if (room === 'living-room') {
+        livingRoomContainer.innerHTML = `Alarm clock at ${time}, alarm ${melody}`;
+    } else if (room === 'kitchen') {
+        kitchenContainer.innerHTML = `Alarm clock at ${time}, alarm ${melody}`;
+    }
 });
 
