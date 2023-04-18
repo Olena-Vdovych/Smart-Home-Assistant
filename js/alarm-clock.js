f21e9510e4513ab732f30c79342bd3e7622ca98c
Olena Vdovych <olena.vdovych2@gmail.com>
2023-04-18 20:19:03 +0300

    Added key for rooms in local storage

--- js/alarm-clock.js
+++ js/alarm-clock.js
@@ -8,19 +8,22 @@ document.getElementById('clock-submit').addEventListener('click', function (e) {
     let time = document.getElementById('time-input').value;
     let melody = document.getElementById('melody-select').options[document.getElementById('melody-select').selectedIndex].textContent;
 
+    let key = room + 'Alarm';
+
     if (room === 'bedroom') {
         bedroomContainer.innerHTML = `Alarm clock at ${time}, alarm ${melody}`;
 
-        localStorage.setItem('bedroomAlarm', bedroomContainer.innerHTML);
+        localStorage.setItem(key, bedroomContainer.innerHTML);
     } else if (room === 'living-room') {
         livingRoomContainer.innerHTML = `Alarm clock at ${time}, alarm ${melody}`;
 
-        localStorage.setItem('livingRoomAlarm', livingRoomContainer.innerHTML);
+        localStorage.setItem(key, livingRoomContainer.innerHTML);
     } else if (room === 'kitchen') {
         kitchenContainer.innerHTML = `Alarm clock at ${time}, alarm ${melody}`;
 
-        localStorage.setItem('kitchenAlarm', kitchenContainer.innerHTML);
+        localStorage.setItem(key, kitchenContainer.innerHTML);
     }
 });
+
 
 
