ac004d7a25c5bcd80430ad112644f7327fdf81af
Olena Vdovych <olena.vdovych2@gmail.com>
2023-04-19 11:28:32 +0300

    Updated functionality for changing channels and text

--- js/tv.js
+++ js/tv.js
@@ -14,10 +14,10 @@ function openTvContainer() {
     const savedChannel = localStorage.getItem('tvChannel');
     if (savedChannel !== null) {
         channelSelect.value = savedChannel;
-        let chanelText = document.querySelector('.chanel-text');
-        chanelText.innerHTML = `You are watching channel ${savedChannel}`;
+        updateChannelText(savedChannel);
     }
 }
+updateSelectedChannelText();
 
 function closeTvContainer() {
     let deviceText = document.querySelector('.device-text');
@@ -27,8 +27,20 @@ function closeTvContainer() {
 
     // Remove saved volume and channel
     localStorage.removeItem('tvVolume');
-    localStorage.removeItem('tvChannel');
+
+    let chanelText = document.querySelector('.chanel-text');
+    chanelText.innerHTML = ''; // Hide the text
 }
+
+function updateSelectedChannelText() {
+    const tvContainer = document.getElementById('tv-container');
+    if (tvContainer.style.display === 'block') {
+        const selectedChannel = channelSelect.value;
+        let chanelText = document.querySelector('.chanel-text');
+        chanelText.innerHTML = `You are watching channel ${selectedChannel}`;
+    }
+}
+
 
 // Changed TV channels and volume
 const volumeUpBtn = document.getElementById('volume-up');
@@ -58,34 +70,29 @@ const channelSelect = document.getElementById('channel-select');
 channelSelect.addEventListener('change', () => {
     const selectedChannel = channelSelect.value;
     localStorage.setItem('tvChannel', selectedChannel);
-    let chanelText = document.querySelector('.chanel-text');
-    chanelText.innerHTML = '';
+    updateChannelText(selectedChannel);
     channelSelect.blur();
 });
 
-// Update the text showing the currently selected channel whenever any button on the page is clicked
+updateSelectedChannelText();
+
+// Update the text showing the currently selected channel
+function updateChannelText(selectedChannel) {
+    let chanelText = document.querySelector('.chanel-text');
+    chanelText.innerHTML = `You are watching channel ${selectedChannel}`;
+}
+
+// Handle clicks on all buttons
 const allButtons = document.querySelectorAll('button');
 allButtons.forEach(button => {
     button.addEventListener('click', event => {
         if (event.target.closest('#tv-container')) {
             const selectedChannel = channelSelect.value;
-            localStorage.setItem('selectedChannel', selectedChannel);
-            let chanelText = document.querySelector('.chanel-text');
-            chanelText.innerHTML = `You are watching channel ${selectedChannel}`;
+            localStorage.setItem('tvChannel', selectedChannel);
+            updateChannelText(selectedChannel);
         } else if (event.target.matches('#close-tv')) {
-            localStorage.removeItem('selectedChannel');
-            let chanelText = document.querySelector('.chanel-text');
-            chanelText.innerHTML = '';
             closeTvContainer();
         }
     });
 });
 
-// Check localStorage for previously selected channel on page load
-window.addEventListener('load', () => {
-    const savedChannel = localStorage.getItem('tvChannel');
-    if (savedChannel !== null) {
-        channelSelect.value = savedChannel;
-        channelSelect.blur();
-    }
-});
